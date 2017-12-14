b9df9f2cd233a25bf54cf5abd7aedd8b09a7fb1b
Rose McCluskey <rmccluskey@hsc.wvu.edu>
2017-12-14 08:31:17 -0500

    Add project files.

---
+++ GettingStartedWithVue/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿
+var growler = new Vue({
+    el: '#growler',
+    data: {
+        appName: 'Growler',
+        query: '',
+        searchIndexes: [],   // for checkbox lists or <select multiple>, items are added/removed to array based on checkbox selections  (e.g. <input type="checkbox" value="beers" v-model="searchIndexes"> <input type="checkbox" value="breweries" v-model="searchIndexes">)
+        searchIndex: 'beers',    // for radio buttons or <select>, bind to singluar/non array property (e.g. <input type="radio" value="beers" v-model="searchIndex">)
+        validation: ''
+    },
+    methods: {
+        executeSearch: function () {
+            if (this.query) {
+                $("#form").submit();
+            } else {
+                this.validation = "Please enter a query.";
+            }
+        },
+        clearValidation: function() {
+            this.validation = '';
+        }
+    }
+});
